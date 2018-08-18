b5c17925bfdf05358600a2c5fb7786b97b2ef281
Raghav Bhasin <raghavbhasin@Raghavs-MacBook-Pro.local>
2018-08-17 20:16:05 -0400

    Added friend relations to expedite adding transactions

--- DrackerAPI/API/users/data/GET/get_users.py
+++ DrackerAPI/API/users/data/GET/get_users.py
@@ -20,11 +20,7 @@ def lambda_handler(event, context):
         response = table.scan(Select='ALL_ATTRIBUTES', FilterExpression=Attr('phone').begins_with(event['search']))['Items']
         result = []
         for item in response:
-            item.pop('email', None)
-            item.pop('customer_url', None)
-            item.pop('funding_source', None)
-            item.pop('friends', None)
-            result.append(item)
+            result.append(process_item(item))
         return json.dumps(result)
     else:
         return get_all_users()
@@ -36,17 +32,17 @@ def get_all_users():
     data = response['Items']
     response = []
     for item in data:
-        item.pop('email', None)
-        item.pop('customer_url', None)
-        item.pop('funding_source', None)
-        item.pop('friends', None)
-        response.append(item)
+        response.append(process_item(item))
     return json.dumps(response)
 
 def get_item_for_key(key):
     item = table.get_item(Key={'phone' : key})['Item']
+    return process_item(item)
+
+def process_item(item):
     item.pop('email', None)
     item.pop('customer_url', None)
     item.pop('funding_source', None)
     item.pop('friends', None)
     return item
+    
