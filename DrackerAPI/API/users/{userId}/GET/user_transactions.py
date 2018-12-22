4bdc495504bcbe7992dc95ce3b760c41ec22175e
RaghavBhasin <raghavbhasin97@gmail.com>
2018-12-22 08:10:55 -0500

    Fixed User's Data Fetch

--- DrackerAPI/API/users/{userId}/GET/user_transactions.py
+++ DrackerAPI/API/users/{userId}/GET/user_transactions.py
@@ -1,69 +1,50 @@
-import boto3
 import json
-from collections import defaultdict
+import boto3
 from datetime import datetime
 
+client = boto3.resource('dynamodb')
+
 def lambda_handler(event, context):
-    path = event['path']
-    query = path.replace('/friends/', '')
-    client = boto3.resource('dynamodb')
-    table = client.Table(query)
-    transactions = table.scan()['Items']
-    friends = defaultdict(list)
-    for item in transactions:
-        friends[item['uid']].append(item)
-    friends_data = {}
-    for key, value in friends.items():
-        details = get_info(value)
-        amount = calculate_amount(value)
-        transactions_list = process_list(value)
-        friends_data[key] = {'phone':details["phone"], 'name':details["name"],'amount':amount,'transactions': transactions_list}
-        
-    return generate_response(friends_data)
+	path = event['path']
+	query = path.replace('/users/', '')
+	print(query)
+	table = client.Table(query)
+	transactions = table.scan()['Items']
+	settled_transactions = []
+	unsettled_transactions = []
+	for item in transactions:
+		try:
+			item['settelement_time'] #Only tring to access this key to throw an error if not setteled yet
+			#Remove unnecessary information if exists
+			item.pop('notification_identifier', None)
+			item.pop('tagged_image', None)
+			item.pop('transaction_id', None)
+			settled_transactions.append(item)
+		except:
+			unsettled_transactions.append(item)
+	credit, debit = calculate_balance(unsettled_transactions)
+	unsettled_transactions = sorted(unsettled_transactions, key=lambda transaction: get_date(transaction['time']), reverse = True)
+	settled_transactions = sorted(settled_transactions, key=lambda transaction: get_date(transaction['settelement_time']), reverse = True)
+	return generate_response({'settled' : settled_transactions, 'unsettled' : unsettled_transactions, 'credit' : credit, 'debit' : debit})
+
 
 def generate_response(response_dictionary):
-    return {'statusCode' : 200,
-            'headers' : {
+	return {'statusCode' : 200,
+			'headers' : {
                 "Content-Type" : "application/json",
                 "Access-Control-Allow-Origin": "*"
             },
             'body' : json.dumps(response_dictionary)
     }
 
-def get_info(transactions_list):
-    data = {}
-    for item in transactions_list:
-        data['phone'] = item['phone']
-        data['name'] = item['name']
-        break
-    return data
-
-def calculate_amount(transactions_list):
-    amount = 0.0
-    for item in transactions_list:
-        try:
-          _ =  item["settelement_time"]
-        except:
-            if item["is_debt"]:
-                amount -= float(item["amount"])
-            else:
-                amount += float(item["amount"])
-    return amount
-def process_list(transactions_list):
-    new_list = []
-    for item in transactions_list:
-        item.pop("tagged_image", None)
-        item.pop("uid", None)
-        item.pop("transaction_id", None)
-        item.pop("phone", None)
-        item.pop("notification_identifier", None)
-        new_list.append(item)
-    return sorted(new_list, key=lambda transaction: sort_key(transaction), reverse = True)
+def calculate_balance(unsettled_transactions):
+	credit = 0.0
+	debit = 0.0
+	for transaction in unsettled_transactions:
+		if transaction['is_debt']:
+			debit += float(transaction['amount'])
+		else:
+			credit += float(transaction['amount'])
+	return (credit,debit)
 def get_date(time_string):
 	return datetime.strptime(time_string, '%m/%d/%y %H:%M:%S.%f %p %z')
-def sort_key(transaction):
-    try:
-        _ = transaction['time']
-        return get_date(transaction['time'])
-    except:
-        return get_date(transaction['settelement_time'])
